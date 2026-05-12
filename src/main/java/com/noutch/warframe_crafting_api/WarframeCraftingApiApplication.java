559da539584daef6aa7c4cfab9f69c92504c7269
noutch11 <129704356+noutch11@users.noreply.github.com>
2026-05-11 22:01:26 -0400

    Initial project set up for a mini Warframe Crafting API with Spring Boot

---
+++ src/main/java/com/noutch/warframe_crafting_api/WarframeCraftingApiApplication.java
@@ -0,0 +1,13 @@
+package com.noutch.warframe_crafting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarframeCraftingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarframeCraftingApiApplication.class, args);
+	}
+
+}
